92468c2f2f5d2e4d4d291ae1fd1a7f08c38f4b97
Lex Do <aleksandr.don256@google.com>
2024-01-27 20:45:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     ],  # List dependencies for automatic installation
     entry_points={
         'console_scripts': [
-            'GrammarHelper=Application.Predict:predict',
+            'GrammarHelper=GrammarHelper.Predict:predict',
             # Replace with your script name, main module, and function
         ],
     },  # Enables running scripts directly (if applicable)
